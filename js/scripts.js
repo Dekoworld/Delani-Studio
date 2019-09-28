9853043aeefb3d02a69073f30d2c7f1e02186151
Dekoworld <karituderrick@gmail.com>
2019-09-28 09:38:49 +0300

    grid page,correct few typos

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+<div class="icons">
+<img src="./img/social_icons/behance.png" alt="icon1">
+<img src="./img/social_icons/dribble.png" alt="icon2">
+<img src="./img/social_icons/facebook.png" alt="icon3">
+<img src="./img/social_icons/flickr.png" alt="icon4">
+<img src="./img/social_icons/g_plus.png" alt="icon5">
+<img src="./img/social_icons/pinterest.png" alt="icon6">
+<img src="./img/social_icons/skype.png" alt="icon7">
+<img src="./img/social_icons/stumble_upon.png" alt="icon8">
+<img src="./img/social_icons/twitter.png" alt="icon9">
+<img src="./img/social_icons/you_tube.png" alt="icon01">
+</div>
+<div class="footer">
+<p>DELANI STUDIO <br>
+2019</p>
+</div>
+
+
+
+<div class="contact">
+          <h4>CONTACT US</h4>
+          <form>
+            <input style="background-color: transparent" type="text" placeholder="Name.." name="Name">
+            <input style="background-color: transparent" type="text" placeholder="Email.." name="Email"><br>
+            <textarea style="background-color: transparent" name="" id="Your message here....." cols="80" rows="5">Your message here</textarea>
+          </form>
+        </div>
+        </div><br><br>
