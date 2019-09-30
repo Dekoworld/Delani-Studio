4aada68b6f069be8bbb6e8f6b955174d80b75e5b
Dekoworld <karituderrick@gmail.com>
2019-09-30 10:33:16 +0300

    add toggle effect to what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,20 @@
 $(document).ready(function(){
-  $("#design").click(function(){
-    $("#design").toggle();
-    $(".p1").toggle();
+  $(".design-c").click(function(){
+    $("#design-image").toggle()
+    $("#design-para").toggle()
   });
-  $(".p1").click(function(){
-     $(".p1").toggle();
-     $("#design").toggle();
-  });
-
-  $("#devpt").click(function(){
-    $("#devpt").hide();
-    $(".p2").show();
-  });
-  $(",p2").click(function(){
-    $(".p2").hide();
-    $("#devpt").show();
+  $(".devpt-c").click(function(){
+    $("#devpt-image").toggle();
+    $("#devpt-para").toggle();
   });
 
-  $("#prdt").click(function(){
-    $("#prdt").hide();
-    $(".p3").show();
+  $(".prdt-c").click(function(){
+    $("#prdt-image").toggle();
+    $("#prdt-para").toggle();
   });
-  $(".p3").click(function(){
-    $(".p3").hide();
-    $("#prdt").show();
-  });
-
-
-
 
   $("form").submit (function(event){
     var name =$("#name").val();
-    alert("Thank you " name + "for you message!Received successfully");
+    alert("Thank you " + name + "for you message!Received successfully");
   });
 });
